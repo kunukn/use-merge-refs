9d7fb205607b871e29bcd9461e337165fd4c74fb
Kunuk Nykjær <kunyk@dfds.com>
2019-10-29 20:56:36 +0100

    upd ver

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,9 @@
 module.exports = {
     // This function will run for each entry/format/env combination
     rollup(config, options) {
-        //options.format = 'umd';
-
-        //console.log(config)
-        ///console.log(options)
-
+        if (options.config === 'cjs') {
+            //options.format = 'umd';
+        }
         return config; // always return a config.
     },
 };
